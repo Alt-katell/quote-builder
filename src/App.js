43a213bc903111ef634a82d03e3a58686991143e
Katell Goaer <alt.katellgoaer@gmail.com>
2020-06-25 19:27:48 -0400

    fetching quotes according to search

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import QuoteCard from './components/QuoteCard/QuoteCard';
 class App extends Component {
   state = {
     quotes: [],
-    selectedQuote: ""
+    selectedQuote: "",
+    search: ""
   };
 
   componentDidMount() {
@@ -24,7 +25,25 @@ class App extends Component {
   }
 
   selectQuoteHandler = (quote) => {
-    this.setState({selectedQuote: quote})
+    this.setState({
+      ...this.state,
+      selectedQuote: quote})
+  }
+
+  searchHandler = (event) => {
+    console.log(event.target.value)
+    this.setState({
+      ...this.state,
+      search: event.target.value
+    })
+    fetch(`https://quote-garden.herokuapp.com/api/v2/quotes/${event.target.value}?page=1&limit=10`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          ...this.state,
+          quotes: data.quotes
+        })
+      })
   }
 
   render() {
@@ -47,7 +66,12 @@ class App extends Component {
             <QuoteGenerator selected={this.state.selectedQuote}/>
           </div>
           <div className="right">
-            <input className="search" type="text" placeholder="Search a quote"/>
+            <input
+              className="search"
+              type="text"
+              placeholder="Search a quote"
+              onChange={this.searchHandler}
+              value={this.state.search}/>
             <ul className="quote-list">
               {quoteList}
             </ul>
