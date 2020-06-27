bfe654da3a69dd22bd6a47ad381e098c8bdb4784
Katell Goaer <alt.katellgoaer@gmail.com>
2020-06-26 20:09:41 -0400

    turned quote generator into a functional component

--- src/components/QuoteGenerator/QuoteGenerator.js
+++ src/components/QuoteGenerator/QuoteGenerator.js
@@ -1,30 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
+
+// import htmlToImage from 'html-to-image';
 
 import './QuoteGenerator.css';
 
-class QuoteGenerator extends Component {
-  render() {
+const QuoteGenerator = (props) => {
     let backgroundColor = {
-      background: this.props.selectedColor
+      background: props.selectedColor
     };
 
     let fontSize = "";
 
-    switch (this.props.selectedFont) {
+    switch (props.selectedFont) {
       case "'Cookie', cursive": fontSize = "28px"; break;
       default: fontSize = "20px";
     }
 
     let fontStyle = {
-      fontFamily: this.props.selectedFont,
+      fontFamily: props.selectedFont,
       fontSize: fontSize
     };
 
     return(
       <div className="quote-generator">
-        <div className="quote-card-build" style={backgroundColor}>
-              <p className="selected-quote" style={fontStyle}>{this.props.selectedQuote.quoteText}</p>
-              <p className="selected-quote-author">{this.props.selectedQuote.quoteAuthor}</p>
+        <div id="quote-card" className="quote-card-build" style={backgroundColor}>
+              <p className="selected-quote" style={fontStyle}>{props.selectedQuote.quoteText}</p>
+              <p className="selected-quote-author">{props.selectedQuote.quoteAuthor}</p>
         </div>
 
         <button className="download-button">Download this quote</button>
@@ -33,26 +34,25 @@ class QuoteGenerator extends Component {
           <div className="choose-color">
             <p>Background color</p>
             <div className="color-options">
-              <div className="color-dot green" onClick={this.props.selectColor}></div>
-              <div className="color-dot pink" onClick={this.props.selectColor}></div>
-              <div className="color-dot yellow" onClick={this.props.selectColor}></div>
-              <div className="color-dot coral" onClick={this.props.selectColor}></div>
-              <div className="color-dot grey" onClick={this.props.selectColor}></div>
+              <div className="color-dot green" onClick={props.selectColor}></div>
+              <div className="color-dot pink" onClick={props.selectColor}></div>
+              <div className="color-dot yellow" onClick={props.selectColor}></div>
+              <div className="color-dot coral" onClick={props.selectColor}></div>
+              <div className="color-dot grey" onClick={props.selectColor}></div>
             </div>
           </div>
 
           <div className="choose-font">
             <p>Font</p>
               <div className="font-options">
-                <p className="font cookie" onClick={this.props.selectFont}>Cookie</p>
-                <p className="font playfair-display" onClick={this.props.selectFont}>Playfair Display</p>
-                <p className="font raleway" onClick={this.props.selectFont}>Raleway</p>
+                <p className="font cookie" onClick={props.selectFont}>Cookie</p>
+                <p className="font playfair-display" onClick={props.selectFont}>Playfair Display</p>
+                <p className="font raleway" onClick={props.selectFont}>Raleway</p>
               </div>
           </div>
         </div>
       </div>
     );
-  }
 }
 
 export default QuoteGenerator;
