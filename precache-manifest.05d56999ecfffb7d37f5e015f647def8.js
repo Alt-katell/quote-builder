d57143d3f6910dc4e975c214c75ee2c4d85006a8
Katell Goaer <alt.katellgoaer@gmail.com>
2020-06-28 18:07:34 -0400

    Updates

---
+++ precache-manifest.05d56999ecfffb7d37f5e015f647def8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6bbd3473001b88f41a83b066eb59649",
+    "url": "/quote-builder/index.html"
+  },
+  {
+    "revision": "9d47b5332e61f12fa08f",
+    "url": "/quote-builder/static/css/main.652944ab.chunk.css"
+  },
+  {
+    "revision": "9b6dbae9c11b8bef18a3",
+    "url": "/quote-builder/static/js/2.a284f514.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-builder/static/js/2.a284f514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d47b5332e61f12fa08f",
+    "url": "/quote-builder/static/js/main.05c3ccc9.chunk.js"
+  },
+  {
+    "revision": "6e2edcce2ab4a7109acc",
+    "url": "/quote-builder/static/js/runtime-main.bca73e46.js"
+  },
+  {
+    "revision": "ccb4e3d696d93d7d80086ed62d793836",
+    "url": "/quote-builder/static/media/feather.ccb4e3d6.svg"
+  }
+]);
